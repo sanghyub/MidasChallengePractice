8010cb83694a91d555bdb3a2e36da875f3af9ec6
sanghyub <sanghyub91@gmail.com>
2017-05-23 20:39:29 +0900

    memoItem

---
+++ app/src/main/java/com/example/sangh/midasparactice/Model/MemoItem.java
@@ -0,0 +1,64 @@
+package com.example.sangh.midasparactice.Model;
+
+import java.util.Date;
+
+/**
+ * Created by sangh on 2017-05-23.
+ */
+
+public class MemoItem {
+    int mNum;
+    String mName;
+    Boolean isImg;
+    String mImagePath;
+    String mContents;
+    Date mDate;
+
+    public int getNum() {
+        return mNum;
+    }
+
+    public void setNum(int num) {
+        mNum = num;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public Boolean getImg() {
+        return isImg;
+    }
+
+    public void setImg(Boolean img) {
+        isImg = img;
+    }
+
+    public String getImagePath() {
+        return mImagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        mImagePath = imagePath;
+    }
+
+    public String getContents() {
+        return mContents;
+    }
+
+    public void setContents(String contents) {
+        mContents = contents;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+}
