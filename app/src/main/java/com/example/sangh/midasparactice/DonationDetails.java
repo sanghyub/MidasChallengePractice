03fe6176838e390fc8d0c6d58c0a2c89f3c2b49d
CheonJiLee <cjswlddl@naver.com>
2017-05-27 14:57:10 +0900

    donation details continue

--- app/src/main/java/com/example/sangh/midasparactice/DonationDetails.java
+++ app/src/main/java/com/example/sangh/midasparactice/DonationDetails.java
@@ -1,8 +1,27 @@
 package com.example.sangh.midasparactice;
 
-/**
- * Created by sangh on 2017-05-27.
- */
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.TextView;
 
-public class DonationDetails {
+public class DonationDetails extends Activity {
+
+    private TextView dTitle;
+    private TextView dContent;
+    private TextView dResult;
+    private Bitmap dImage;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.donation_details);
+
+        dTitle = (TextView)findViewById(R.id.donationTitle);
+        dContent = (TextView)findViewById(R.id.donationContent);
+        dResult = (TextView)findViewById(R.id.donationResult);
+
+    }
 }
